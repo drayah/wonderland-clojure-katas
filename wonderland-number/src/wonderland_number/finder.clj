53d35a087976f0fdc1ef54c08f937ff4c714ae58
Giovanni Martina <gio@popado.co>
2017-04-24 12:52:42 -0300

    Add solution for wonderland-number kata

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,23 @@
 (ns wonderland-number.finder)
 
-(defn wonderland-number []
-  ;; calculate me
-  42)
+(def candidates (range 100000 1000000))
+
+(defn same
+  "Given numbers return true if they have 
+  the same digits irrespective of order"
+  [& nums]
+  (let [sets (map #(set (str %)) nums)]
+    (apply = sets)))
+
+(defn wonderland-number
+  "Generate wonderland nubmer"
+  []
+  (let [number (for [n candidates
+                     :let [n2 (* 2 n)
+                           n3 (* 3 n)
+                           n4 (* 4 n)
+                           n5 (* 5 n)
+                           n6 (* 6 n)]
+                     :when (same n n2 n3 n4 n5 n6)]
+                  n)]
+    (first number)))
--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -7,6 +7,22 @@
         s2 (set (str n2))]
     (= s1 s2)))
 
+(deftest test-candidates
+  (testing "Should have a range of candidates"
+    (is (= 100000 (first candidates)))
+    (is (= 999999 (last candidates)))))
+
+(deftest test-same
+  (testing "Numbers with same digits should be the same"
+    (is (true? (same 12345 52431 12354 12354 12354 12354 12354)))
+    (is (true? (same 99999 99999)))
+    (is (true? (same 767 776 677)))
+    (is (false? (same 123 124)))
+    (is (true? (same 1)))
+    (is (true? (same 1 1 1)))
+    (is (false? (same 1 1 1 4)))
+    (is (false? (same 333 444)))))
+
 (deftest test-wonderland-number
   (testing "A wonderland number must have the following things true about it"
     (let [wondernum (wonderland-number)]
