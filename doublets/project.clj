47b9dfbfa15d7a5464d7d62eb8714d7c7fc30c3b
Giovanni Martina <gio@popado.co>
2017-05-17 21:48:21 -0300

    Update lein-test-refresh dependency

--- doublets/project.clj
+++ doublets/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}}
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]}}
   :main doublets.solver)
